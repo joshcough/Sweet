c04a6aacfd8007b613d9b57ebe1fda4a64bfdd29
unknown <HP_Administrator@.(none)>
2009-11-02 23:36:52 -0600

    fixed minor issue with assertions.

--- src/main/scala/sweet/SweetEvent.scala
+++ src/main/scala/sweet/SweetEvent.scala
@@ -3,6 +3,6 @@ package sweet
 trait SweetEvent
 
 case class TestStarting(testName: String) extends SweetEvent
-case class TestFailed(testName: String, reason:Throwable) extends SweetEvent// AssertionError?
+case class TestFailed(testName: String, reason:SourAssertionException) extends SweetEvent
 case class TestErrored(testName: String, reason:Throwable) extends SweetEvent
 case class TestSucceeded(testName: String) extends SweetEvent
