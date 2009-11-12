b3fde03669c8e39cc333cf87e7d9f0a6b9439252
unknown <HP_Administrator@.(none)>
2009-11-12 00:07:36 -0600

    added TestNGSweet, ability to write tests in TestNG style, run in TestNG IDE runner.

--- project/build/SweetProject.scala
+++ project/build/SweetProject.scala
@@ -11,4 +11,5 @@ class SweetProject(info: ProjectInfo) extends DefaultProject(info) with AutoComp
   val snapshotsRepository = "Scala Tools Repository" at "http://scala-tools.org/repo-snapshots"  
   val testInterfaces = "org.scala-tools.testing" % "test-interface" % "0.1"
 
+  val testng = "org.testng" % "testng" % "5.7" from "http://repo1.maven.org/maven2/org/testng/testng/5.7/testng-5.7-jdk15.jar"
 }
