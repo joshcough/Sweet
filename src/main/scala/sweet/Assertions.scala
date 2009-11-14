1fda76ba649aa951e2cd11b145f927adf02e999c
Josh Cough <joshcough@gmail.com>
2009-11-13 22:32:05 -0600

    minor cosmetics

--- src/main/scala/sweet/Assertions.scala
+++ src/main/scala/sweet/Assertions.scala
@@ -3,13 +3,12 @@ package sweet
 trait Assertions {
 
   case class Equalizer(a:Any){
-    def mustBe(b:Any){
-      if( ! a.equals(b) ) throw new SourAssertionException(a + " did not equal " + b + " but should have")
-    }
 
-    def mustNotBe(b:Any) {
-      if( a.equals(b) ) throw new SourAssertionException(a + " must not equal " + b + ", but did")
-    }
+    def mustBe(b:Any){ if( ! a.equals(b) )toss(a + " did not equal " + b + " but should have") }
+
+    def mustNotBe(b:Any) { if( a.equals(b) ) toss(a + " must not equal " + b + ", but did") }
+
+    def toss(message: String){ throw new SourAssertionException(message) }
   }
 
   implicit def Any2Equalizer(a: Any) = Equalizer(a)
--- src/main/scala/sweet/SweetRunner.scala
+++ src/main/scala/sweet/SweetRunner.scala
@@ -3,6 +3,7 @@ package sweet
 import org.scalatools.testing._
 
 class SweetRunner(val classLoader: ClassLoader, loggers: Array[Logger]) extends Runner {
+
   def run(testClassName: String, fingerprint: TestFingerprint, eventHandler: EventHandler, args: Array[String]){
     val testClass = Class.forName(testClassName, true, classLoader).asSubclass(classOf[Sweet])
     val sweet = testClass.newInstance
@@ -12,25 +13,14 @@ class SweetRunner(val classLoader: ClassLoader, loggers: Array[Logger]) extends 
 
   class MySweetReporter(eventHandler: EventHandler) extends SweetReporter with NotNull{
 
-    private def logError(msg: String) = loggers.foreach(_ error msg)
-    private def logWarn(msg: String) = loggers.foreach(_ warn msg)
-    private def logInfo(msg: String) = loggers.foreach(_ info msg)
-    private def logDebug(msg: String) = loggers.foreach(_ debug msg)
-
     def newEvent(tn: String, r: Result, e: Option[Throwable]) {
-      eventHandler.handle(new org.scalatools.testing.Event {
-        def testName = tn
-        def description = tn
-        def result = r
-        def error = e getOrElse null
-      })
+      class MyEvent(val testName:String, val description:String, val result:Result, val error:Throwable) extends Event
+      eventHandler.handle(new MyEvent(tn, tn, r, e getOrElse null))
     }
 
     def apply(event: SweetEvent) {
       event match {
-      // just log this
-        case t: TestStarting => logInfo("Test Starting: " + t.testName)
-        // the results of running an actual test
+        case t: TestStarting =>  loggers.foreach(_ info "Test Starting: " + t.testName)
         case t: TestFailed => newEvent(t.testName, Result.Failure, Some(t.reason))
         case t: TestErrored => newEvent(t.testName, Result.Failure, Some(t.reason))
         case t: TestSucceeded => newEvent(t.testName, Result.Success, None)
